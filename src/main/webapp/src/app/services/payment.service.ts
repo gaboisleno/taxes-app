04704d5c741474fbe5b9698eaf7c5b7e9bfde90e
gaboisleno <isleno.gabriel@gmail.com>
2024-06-20 15:19:44 -0300

    public endpoint payment

--- src/main/webapp/src/app/services/payment.service.ts
+++ src/main/webapp/src/app/services/payment.service.ts
@@ -7,7 +7,7 @@ import { environment } from '../../environments/environment';
   providedIn: 'root',
 })
 export class PaymentService {
-  private entityUrl = '/private/payment';
+  private entityUrl = '/public/payment';
   private apiUrl = environment.apiUrl + this.entityUrl;
 
   constructor(protected http: HttpClient) {}
